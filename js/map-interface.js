b175463c8028620da31697b488427f10919e7644
Jeremy Fryd and Sandro Alvarez <jeremy+sandro@>
2016-10-12 16:14:48 -0700

    fix everything

--- js/map-interface.js
+++ js/map-interface.js
@@ -1,9 +1,11 @@
 var MapMaker = require("./../js/map.js").mapModule;
 var googleKey = require("./../.env").googleKey;
 
-$(function() {
-  $("#google-script").html("<script src=https://maps.googleapis.com/maps/api/js?key="+googleKey+"></script>");
+$(document).ready(function() {
+  $("head").append("<script src=https://maps.googleapis.com/maps/api/js?key="+googleKey+"></script>");
   var map = new MapMaker();
-  map.initMap();
-  // $("#map").html(map.map);
+
+  $("#map-init").click(function() {
+    map.initiateMap();
+  });
 });
--- js/map.js
+++ js/map.js
@@ -4,15 +4,12 @@ function MapMaker() {
   this.map;
 }
 
-MapMaker.prototype.initMap = function() {
-  // $.get("https://maps.googleapis.com/maps/api/js?key=" + googleKey).then(function(response) {
-    this.map = new google.maps.Map(document.getElementById("map"), {
-      center: {lat: 45.5207, lng: -122.6775},
-      zoom: 20
-    });
-  // }).fail(function(error) {
-  //   $('#map').text("NOT WORKING");
-  // });
+MapMaker.prototype.initiateMap = function() {
+  this.map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 45.5207, lng: -122.6775},
+    scrollwheel: true,
+    zoom: 18
+  });
 };
 
 exports.mapModule = MapMaker;
